81ec7db02824081bc1225ce147106a4e71de14ee
Jillboy <jillboy@protonmail.com>
2017-06-19 00:16:47 -0300

    Update to ES6 (why not? every browser supports it)

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 /**
  * Search DuckDuckGo on Enter key press or search button click.
  */
-document.getElementById('search-input').addEventListener("keyup", function (event) {
-  if (event.key === 'Enter') {
+document.getElementById('search-input').onkeyup = (event) => {
+  if (event.key === 'Enter')
     document.getElementById('search-button').click();
-  }
-});
-document.getElementById('search-button').addEventListener("click", function (event) {
+}
+document.getElementById('search-button').onclick = (event) => {
   const query = document.getElementById('search-input').value;
   window.location.href = 'https://duckduckgo.com/?q=' + query;
-});
-
-/**
- * Table of links.
- */
-const link = document.getElementsByClassName('link');
-for (let i = 0; i < link.length; i++) {
-  link[i].onclick = function (event) {
-    window.location.href = this.getAttribute('data-url');
-  }
 }
 
 /**
