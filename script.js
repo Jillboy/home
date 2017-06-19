96744c91bfbec459796fb9c763384f6d7af5d98e
Jillboy <jillboy@protonmail.com>
2017-06-19 00:01:01 -0300

    Add link redirection functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /**
- * Search DuckDuckGo on Enter key press or search button click
+ * Search DuckDuckGo on Enter key press or search button click.
  */
 document.getElementById('search-input').addEventListener("keyup", function (event) {
   if (event.key === 'Enter') {
@@ -7,9 +7,19 @@ document.getElementById('search-input').addEventListener("keyup", function (even
   }
 });
 document.getElementById('search-button').addEventListener("click", function (event) {
-  var query = document.getElementById('search-input').value;
+  const query = document.getElementById('search-input').value;
   window.location.href = 'https://duckduckgo.com/?q=' + query;
 });
+
+/**
+ * Table of links.
+ */
+const link = document.getElementsByClassName('link');
+for (let i = 0; i < link.length; i++) {
+  link[i].onclick = function (event) {
+    window.location.href = this.getAttribute('data-url');
+  }
+}
 
 /**
  * Get list of online streamers that are followed.
