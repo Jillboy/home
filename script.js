21fe61fdfe522596bc352ffab5f1ed8d2cfff235
Jillboy <jillboy@protonmail.com>
2017-06-19 05:38:50 -0300

    Major overhaul, let's just use dropdown menus

--- script.js
+++ script.js
@@ -7,16 +7,3 @@ document.getElementById('search-button').onclick = (event) => {
   const query = document.getElementById('search-input').value;
   window.location.href = 'https://duckduckgo.com/?q=' + query;
 }
-
-// Open link in new tab on clicking a link cell.
-const links = document.getElementsByClassName('link');
-for (let i = 0; i < links.length; i++) {
-  links[i].onclick = (event) => {
-    window.open(links[i].getAttribute('data-url'));
-  }
-}
-
-/**
- * Get list of online streamers that are followed.
- */
-// TODO.
