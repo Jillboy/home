b47b3875ccf10e82eea82a0f86a02ada45a5e614
Jillboy <jillboy@protonmail.com>
2017-06-19 00:29:17 -0300

    Add initial link functionality

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-/**
- * Search DuckDuckGo on Enter key press or search button click.
- */
+// Search DuckDuckGo on Enter key press or search button click.
 document.getElementById('search-input').onkeyup = (event) => {
   if (event.key === 'Enter')
     document.getElementById('search-button').click();
@@ -8,6 +6,14 @@ document.getElementById('search-input').onkeyup = (event) => {
 document.getElementById('search-button').onclick = (event) => {
   const query = document.getElementById('search-input').value;
   window.location.href = 'https://duckduckgo.com/?q=' + query;
+}
+
+// Redirect to appropriate page on clicking a link cell.
+const links = document.getElementsByClassName('link');
+for (let i = 0; i < links.length; i++) {
+  links[i].onclick = (event) => {
+    window.location.href = links[i].getAttribute('data-url');
+  }
 }
 
 /**
